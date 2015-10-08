b146f0b0a216651d276ffdf38c5339fc8ee7d705
Anthony Fry <antfry256@gmail.com>
2015-10-08 16:33:23 +1100

    AI added Medium

--- src/GameController.cs
+++ src/GameController.cs
@@ -76,8 +76,11 @@ public static class GameController
 
 		//create the players
 		switch (_aiSetting) {
-			case AIOption.Medium:
+			case AIOption.Easy:
 				_ai = new AIEasyPlayer(_theGame);
+				break;
+			case AIOption.Medium:
+				_ai = new AIMediumPlayer(_theGame) ;
 				break;
 			case AIOption.Hard:
 				_ai = new AIHardPlayer(_theGame);
